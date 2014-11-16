88dad501346d36b83b2fae0d248a0e2cdbdff09c
Davide <davide.rambaldi@gmail.com>
2014-11-16 15:10:56 +0100

    metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'base-server'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures base-server'
+maintainer       'davide.rambaldi@gmail.com'
+maintainer_email 'davide.rambaldi@gmail.com'
+license          'apachev2'
+description      'Installs/Configures a server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
